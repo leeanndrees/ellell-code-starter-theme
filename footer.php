fdce7b81a00276b9adfae76068948eee852e9400
LeeannDrees <leeann.drees@gmail.com>
2018-08-09 15:35:43 -0400

    initial commit - copy of starter from Design + Code workshop

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ **/
+?>
+
+<footer class="site-footer container-fluid bg-color">
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-sm-6 col-sm-offset-3 text-center">
+
+        <?php dynamic_sidebar( 'footer' ); ?>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</footer>
+
+</body>
+
+<?php wp_footer(); ?>
+</html>
