84cf0d341c0ccc9cd3edb5a27bfdb3600718b6c1
LeeannDrees <leeann.drees@gmail.com>
2018-08-10 11:16:43 -0400

    removing homepage widget area

--- functions.php
+++ functions.php
@@ -20,43 +20,37 @@ add_action( 'init', 'ellelltheme_menus' );
 
 /********** Custom Excerpt **************************************************/
 
-// function ellelltheme_excerpt($more) {
-//   return ' &hellip; <a href="'.get_permalink().'">Continue Reading &rarr;</a>';
-// }
-// add_filter('excerpt_more', 'ellelltheme_excerpt');
+function ellelltheme_excerpt($more) {
+  return ' &hellip; <a href="'.get_permalink().'">Continue Reading &rarr;</a>';
+}
+add_filter('excerpt_more', 'ellelltheme_excerpt');
 
 
 /********** Registering widget areas **************************************************/
 
-// function ellelltheme_custom_sidebars() {
-// 	$args = array(
-// 		'id'            => 'homepage',
-// 		'name'          => __( 'Homepage Widget Area' ),
-// 		'description'   => __( 'Appears beneath blog posts on homepage' ),
-// 	);
-// 	register_sidebar( $args );
-//
-//   $args = array(
-// 		'id'            => 'footer1',
-// 		'name'          => __( 'Footer 1' ),
-// 		'description'   => __( 'Footer widget area 1 – appears in left column' ),
-// 	);
-// 	register_sidebar( $args );
-//
-//   $args = array(
-// 		'id'            => 'footer2',
-// 		'name'          => __( 'Footer 2' ),
-// 		'description'   => __( 'Footer widget area 2 – appears in middle column' ),
-// 	);
-// 	register_sidebar( $args );
-//
-//   $args = array(
-// 		'id'            => 'footer3',
-// 		'name'          => __( 'Footer 3' ),
-// 		'description'   => __( 'Footer widget area 3 – appears in right column' ),
-// 	);
-// 	register_sidebar( $args );
-// }
-// add_action( 'widgets_init', 'ellelltheme_custom_sidebars' );
+function ellelltheme_custom_sidebars() {
+  
+  $args = array(
+		'id'            => 'footer1',
+		'name'          => __( 'Footer 1' ),
+		'description'   => __( 'Footer widget area 1 – appears in left column' ),
+	);
+	register_sidebar( $args );
+
+  $args = array(
+		'id'            => 'footer2',
+		'name'          => __( 'Footer 2' ),
+		'description'   => __( 'Footer widget area 2 – appears in middle column' ),
+	);
+	register_sidebar( $args );
+
+  $args = array(
+		'id'            => 'footer3',
+		'name'          => __( 'Footer 3' ),
+		'description'   => __( 'Footer widget area 3 – appears in right column' ),
+	);
+	register_sidebar( $args );
+}
+add_action( 'widgets_init', 'ellelltheme_custom_sidebars' );
 
 ?>
